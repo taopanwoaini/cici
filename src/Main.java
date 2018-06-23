1295ff011cb0dcd6ba893564aebadc239ee655e7
taopanwoaini <2540006753@qq.com>
2018-06-23 21:10:17 +0800

    删除

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        //再一次
         System.out.println("idea,xiugai");
+
     }
 }
